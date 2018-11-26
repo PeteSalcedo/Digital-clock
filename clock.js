a397021883e159070216fb1015934437245ff8b5
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-11-26 15:18:19 -0600

    Uploading th final touches to the clock updates without api

--- clock.js
+++ clock.js
@@ -1,1 +1,28 @@
-alert("Get out now")
+function bigben(){
+
+const fullDate = new Date();
+
+ let hours = fullDate.getHours();
+ let mins = fullDate.getMinutes();
+ let secs = fullDate.getSeconds();
+
+ if(hours < 10){
+     hours = '0' + hours;
+ }
+
+
+ if(mins < 10){
+    mins = '0' + mins;
+}
+
+if(secs < 10){
+    secs = '0' + secs;
+}
+ 
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHTML = ": " + mins + " : ";
+document.getElementById('second').innerHTML = secs;
+
+}
+
+setInterval(bigben,1000); 
