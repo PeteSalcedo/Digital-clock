067c89c508cbb04c229621b06f96435b2e1fd4a6
petesalcedo <peter.salcedo.diaz@gmail.com>
2021-12-31 03:02:13 -0500

    adding the api call for nasa image of the day

--- clock.js
+++ clock.js
@@ -26,3 +26,17 @@ document.getElementById('second').innerHTML = secs;
 }
 
 setInterval(bigben,1000); 
+
+let apikey = "";
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apikey}`;
+
+async function getImage(url){
+  const response = await fetch(url);
+  let data = await response.json()
+  console.log(data.url)
+  if(data.url !== ''){
+    document.getElementById('imageofday').src= data.url
+  }
+}
+
+getImage(url)
